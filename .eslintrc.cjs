f4fff49d8cda3c7fddc1413501f328bf99ce131f
Tommy Leung <me@supertommy.com>
2022-10-14 13:26:37 -0400

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,19 @@
 module.exports = {
-	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	overrides: [],
 	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 	plugins: ['@typescript-eslint'],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/eslint-recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
 	rules: {
 		'@typescript-eslint/explicit-function-return-type': 0,
 		'@typescript-eslint/ban-ts-ignore': 0,
-		'@typescript-eslint/no-namespace': { allowDeclarations: true },
 		'@typescript-eslint/member-delimiter-style': 0,
 		'@typescript-eslint/no-explicit-any': 0,
 	},
