4a1ffc3ed0a8a927908d03ea03ce9157f8a60638
Megan Englert <megan.adare@live.com>
2023-01-20 17:05:46 -1000

    properly displays colorwheel

--- src/components/Shop.ts
+++ src/components/Shop.ts
@@ -3,6 +3,19 @@ import Phaser from 'phaser'
 export default class Shop extends Phaser.GameObjects.Container{
     
     private coins: number
+
+    /**** READ ME from Megan
+     * So... the fix is to put the call to load the image
+     * in the GameScene.ts preload function. 
+     * 
+     * That will then load up the image and set a global name
+     * for it (in this case, color)
+     * 
+     * You can then freely reference it in any component
+     * 
+     * Since I had Canva open already, I made your transparent
+     * actually transparent (it had the grid showing on it)
+     */
     constructor(scene: Phaser.Scene) {
         super(scene)
         //X and Y coords
@@ -10,6 +23,7 @@ export default class Shop extends Phaser.GameObjects.Container{
         //this.y=
 
         this.coins = 0;
+
         //needs sizing and placement figured out
         //add different images/text the same way you would with create()
 
@@ -20,6 +34,7 @@ export default class Shop extends Phaser.GameObjects.Container{
         //last lines here
         this.scene.add.existing(this);
     }
+
 
     earnCoin() {
         this.coins++;
