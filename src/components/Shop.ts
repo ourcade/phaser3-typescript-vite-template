d21a8f4c77d4bf4c62111b5d0dc1830bc92c7085
Lucas Castle <lucas@Lucass-MacBook-Air-3.local>
2023-01-20 15:55:37 -1000

    Pushed to figure out problems

--- src/components/Shop.ts
+++ src/components/Shop.ts
@@ -1,7 +1,8 @@
 import Phaser from 'phaser'
-
 export default class Shop extends Phaser.GameObjects.Container{
-    
+    preload(){
+        this.load.image('color','assets/Colorwheel.png')
+    }
     private coins: number
     constructor(scene: Phaser.Scene) {
         super(scene)
@@ -12,14 +13,15 @@ export default class Shop extends Phaser.GameObjects.Container{
         this.coins = 0;
         //needs sizing and placement figured out
         //add different images/text the same way you would with create()
-
+       
         this.add(this.scene.add.text(100,100, "example text", {fontSize: '28px'}))
-
+        this.add(this.scene.add.image(100,150,'color'))
 
         //last lines here
         this.scene.add.existing(this);
     }
-
+    
+  
     earnCoin() {
         this.coins++;
     }
