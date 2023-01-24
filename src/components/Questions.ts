6d36c3a3fd70ab349adae817735c7c873d81c662
rrachelr <rachrobins1@gmail.com>
2023-01-24 13:07:33 -1000

    Question formatting

--- src/components/Questions.ts
+++ src/components/Questions.ts
@@ -1,9 +1,9 @@
 import Phaser from 'phaser'
 import GameScene from "../GameScene"
-export default class Questions extends Phaser.GameObjects.Container{
+export default class Questions extends Phaser.GameObjects.Container {
     
     //Rachel
-    private popup?: Phaser.GameObjects.Image;
+    private popup: Phaser.GameObjects.Image;
     private contain: Phaser.GameObjects.Container | undefined;
     private quiztext: Phaser.GameObjects.Text | undefined;
     private questions: Array<string>;
@@ -13,22 +13,22 @@ export default class Questions extends Phaser.GameObjects.Container{
 
     constructor(scene: GameScene) { //don't touch
         super(scene) //Don't touch
-        this.questions = ["Nene color is an example of an attribute.", "Hats are an example of a subclass.", "Our nene is an instance of a class."];
-        this.popup = this.scene.physics.add.image(0, 0, 'popup').setOrigin(0);
-        this.quiztext = this.scene.add.text(30, 34, "this is a test :)")
+        this.questions = ["Color is an example of an attribute.", "Hats are an example of a subclass.", "Our nene is an instance of a class."];
+        this.popup = this.scene.physics.add.image(150,100, 'popup').setOrigin(0);
+        this.quiztext = this.scene.add.text(200, 150, "this is a test :)")
             .setColor('#000000');
-        this.answerT = this.scene.add.text(50, 100, "true")
+        this.answerT = this.scene.add.text(250, 275, "true")
             .setColor('#000000')
             .setInteractive()
 			.on('pointerover', () => this.answerT?.setColor('#fff000'))
 			.on('pointerout', () => this.answerT?.setColor('#000000'));
-        this.answerF = this.scene.add.text(300, 100, "false")
+        this.answerF = this.scene.add.text(400, 275, "false")
             .setColor('#000000')
             .setInteractive()
 			.on('pointerover', () => this.answerF?.setColor('#fff000'))
 			.on('pointerout', () => this.answerF?.setColor('#000000'));
 		this.contain = this.scene.add.container(32, 70, [ this.popup, this.quiztext, this.answerT, this.answerF ]);
-		this.contain.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.popup.width, this.popup.height), Phaser.Geom.Rectangle.Contains);
+		//this.contain.setInteractive(new Phaser.Geom.Rectangle(350, 600, this.popup.width, this.popup.height), Phaser.Geom.Rectangle.Contains);
         this.quizPopUp();
         this.scene.add.existing(this); //Don't touch
     }
@@ -49,8 +49,4 @@ export default class Questions extends Phaser.GameObjects.Container{
         }
     }
 
-    //methods down here
-    public earnCoin() {
-        return 0
-    }
 }
