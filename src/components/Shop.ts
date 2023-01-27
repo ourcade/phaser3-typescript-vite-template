e92afa3a54266c59586f5f896114ad3cc0eb4994
Lucas Castle <lucas@Lucass-MacBook-Air-3.local>
2023-01-26 22:48:17 -1000

    Updated Counter for total nenes collected, also I understand the text now

--- src/components/Shop.ts
+++ src/components/Shop.ts
@@ -13,11 +13,11 @@ export default class Shop extends Phaser.GameObjects.Container{
     constructor(scene: Phaser.Scene) {
         super(scene)
         
-        this.add(this.scene.add.text(40,240, "Additional Colors", {fontSize: '18px',"color":"0x000000"}))
-        this.add(this.scene.add.text(60,415, "Baseball Caps", {fontSize: '18px',"color":"0x000000"}))
-        this.add(this.scene.add.text(80,260, "5 Coins", {fontSize: '18px',"color":"0x000000"}))
-        this.add(this.scene.add.text(80,440, "3 Coins", {fontSize: '18px', "color":"0x000000"}))
-        this.scoreText = this.scene.add.text(20,550,"Coins: 10", {fontSize: '32px',"color":"0x000000"});
+        this.add(this.scene.add.text(40,240, "Additional Colors", {fontSize: '18px',"color":"#FFFFFF"}))
+        this.add(this.scene.add.text(60,415, "Baseball Caps", {fontSize: '18px',"color":"#FFFFFF"}))
+        this.add(this.scene.add.text(80,260, "5 Coins", {fontSize: '18px',"color":"#FFFFFF"}))
+        this.add(this.scene.add.text(80,440, "3 Coins", {fontSize: '18px', "color":"#FFFFFF"}))
+        this.scoreText = this.scene.add.text(20,550,"Coins: 10", {fontSize: '32px',"color":"#FFFFFF"});
         this.color = this.scene.add.image(125,175,'color')
         this.color.setScale(0.25)
         this.hats = this.scene.add.image(125,350,'hats')
@@ -28,13 +28,13 @@ export default class Shop extends Phaser.GameObjects.Container{
             (this.scene as GameScene).coins = (this.scene as GameScene).coins-5;
             this.scoreText.setText(`Coins: ${(this.scene as GameScene).coins}`);
             this.color.removeInteractive()
-            this.add(this.scene.add.text(80,175, "SOLD OUT!",  {fontSize: '18px'}))
+            this.add(this.scene.add.text(65,175, "SOLD OUT!",  {fontSize: '24px',"color":"#FFFFFF"}))
         })
         this.hats.on('pointerup',() =>{
             (this.scene as GameScene).coins = (this.scene as GameScene).coins-3;
                this.scoreText.setText(`Coins: ${(this.scene as GameScene).coins}`);
                 this.hats.removeInteractive()
-                this.add(this.scene.add.text(85,340, "SOLD OUT!", {fontSize: '18px'}))
+                this.add(this.scene.add.text(65,340, "SOLD OUT!", {fontSize: '24px',"color":"#ff0303"}))
             })
 
         //last lines here
